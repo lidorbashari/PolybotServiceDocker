4782503c2e9d99aabc7c4f427ecbceae6af08453
lidor <lidor1965@gmail.com>
2025-01-12 14:51:41 +0200

    9' try

--- polybot/bot.py
+++ polybot/bot.py
@@ -23,7 +23,7 @@ class Bot:
         time.sleep(0.5)
 
         # set the webhook URL
-        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
+        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{telegram_token}/', timeout=60)
 
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
 
