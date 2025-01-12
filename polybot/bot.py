e1071435b0e83ac7b4f379c90a7fad7deef1dd9a
lidor <lidor1965@gmail.com>
2025-01-12 15:14:25 +0200

    10' try

--- polybot/bot.py
+++ polybot/bot.py
@@ -8,7 +8,6 @@ import boto3
 
 
 class Bot:
-
     def __init__(self, telegram_token, telegram_chat_url, s3_bucket_name, s3_region, s3_client=None):
         self.telegram_bot_client = telebot.TeleBot(telegram_token)
         self.S3_BUCKET_NAME = s3_bucket_name
