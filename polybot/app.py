1fcf815be36491c969ae520011278e5866a3a4eb
lidor <lidor1965@gmail.com>
2025-01-12 13:19:28 +0200

    3' try

--- polybot/app.py
+++ polybot/app.py
@@ -12,10 +12,8 @@ TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 S3_REGION = os.environ['S3_REGION']
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 EC2_URL = os.environ['EC2_URL']
-s3_client = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID,aws_secret_access_key=AWS_SECRET_ACCESS_KEY,region_name=S3_REGION)
+s3_client = boto3.client('s3')
 bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL, S3_BUCKET_NAME, S3_REGION, EC2_URL, s3_client)
 
 
