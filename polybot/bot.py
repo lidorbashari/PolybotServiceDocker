5802fcd60472c59c672798a0e222d33b0e9d0ab1
lidor <lidor1965@gmail.com>
2025-01-17 10:28:26 +0200

    finish project

--- polybot/bot.py
+++ polybot/bot.py
@@ -73,7 +73,7 @@ class ObjectDetectionBot(Bot):
         if self.is_current_msg_photo(msg):
             self.process_photo_message(msg)
         else:
-            self.send_text(msg['chat']['id'], "שישו שלח לי תמונה")
+            self.send_text(msg['chat']['id'], "Please send a picture")
 
     def process_photo_message(self, msg):
         try:
